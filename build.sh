f214850808acfa6eb8a098e51232e50661fdf449
bggRGjQaUbCoE <githubaccount56556@proton.me>
2023-04-05 19:13:14 +0800

    new

---
+++ build.sh
@@ -0,0 +1,10 @@
+config=vendor/y2q_chn_openx_defconfig
+export PATH="/home/van/clang10/bin:/home/van/GCC_4.9/aarch64-linux-android-4.9/bin:/home/van/GCC_4.9/arm-linux-androideabi-4.9/bin:$PATH"
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-android-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+
+make -j12 CC=clang O=out $config
+
+# make -j12 CC=clang O=out
