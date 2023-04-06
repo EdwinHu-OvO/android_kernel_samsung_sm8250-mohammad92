c50d0bfefcb583c8c1b83341ed189697f928145b
bggRGjQaUbCoE <githubaccount56556@proton.me>
2023-04-06 10:03:29 +0800

    Revert "platform/x86: acer-wmi: Cleanup accelerometer device handling"
    This reverts commit 1431317c0064022c67a1f6426636d67a4c7f7326.

--- drivers/platform/x86/acer-wmi.c
+++ drivers/platform/x86/acer-wmi.c
@@ -224,6 +224,7 @@ struct hotkey_function_type_aa {
 #define ACER_CAP_BLUETOOTH		BIT(2)
 #define ACER_CAP_BRIGHTNESS		BIT(3)
 #define ACER_CAP_THREEG			BIT(4)
+#define ACER_CAP_ACCEL			BIT(5)
 
 /*
  * Interface type flags
@@ -1525,7 +1526,7 @@ static int acer_gsensor_event(void)
 	struct acpi_buffer output;
 	union acpi_object out_obj[5];
 
-	if (!acer_wmi_accel_dev)
+	if (!has_cap(ACER_CAP_ACCEL))
 		return -1;
 
 	output.length = sizeof(out_obj);
@@ -1934,6 +1935,8 @@ static int __init acer_wmi_accel_setup(void)
 	if (err)
 		return err;
 
+	interface->capability |= ACER_CAP_ACCEL;
+
 	acer_wmi_accel_dev = input_allocate_device();
 	if (!acer_wmi_accel_dev)
 		return -ENOMEM;
@@ -1957,6 +1960,11 @@ static int __init acer_wmi_accel_setup(void)
 err_free_dev:
 	input_free_device(acer_wmi_accel_dev);
 	return err;
+}
+
+static void acer_wmi_accel_destroy(void)
+{
+	input_unregister_device(acer_wmi_accel_dev);
 }
 
 static int __init acer_wmi_input_setup(void)
@@ -2113,7 +2121,7 @@ static int acer_resume(struct device *dev)
 	if (has_cap(ACER_CAP_BRIGHTNESS))
 		set_u32(data->brightness, ACER_CAP_BRIGHTNESS);
 
-	if (acer_wmi_accel_dev)
+	if (has_cap(ACER_CAP_ACCEL))
 		acer_gsensor_init();
 
 	return 0;
@@ -2321,8 +2329,8 @@ error_device_alloc:
 error_platform_register:
 	if (wmi_has_guid(ACERWMID_EVENT_GUID))
 		acer_wmi_input_destroy();
-	if (acer_wmi_accel_dev)
-		input_unregister_device(acer_wmi_accel_dev);
+	if (has_cap(ACER_CAP_ACCEL))
+		acer_wmi_accel_destroy();
 
 	return err;
 }
@@ -2332,8 +2340,8 @@ static void __exit acer_wmi_exit(void)
 	if (wmi_has_guid(ACERWMID_EVENT_GUID))
 		acer_wmi_input_destroy();
 
-	if (acer_wmi_accel_dev)
-		input_unregister_device(acer_wmi_accel_dev);
+	if (has_cap(ACER_CAP_ACCEL))
+		acer_wmi_accel_destroy();
 
 	remove_debugfs();
 	platform_device_unregister(acer_platform_device);
